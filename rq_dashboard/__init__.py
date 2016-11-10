e8fc4e85e3491860fa069a8058787eefbcf60360
sam <sam@untapt.com>
2016-11-09 21:43:14 -0500

    remove logging

--- rq_dashboard/__init__.py
+++ rq_dashboard/__init__.py
@@ -1,13 +1,3 @@
 # flake8: noqa
 from . import default_settings
 from .web import blueprint
-import logging
-
-try: 
-    from logging import NullHandler
-except ImportError:
-    class NullHandler(logging.Handler):
-        def emit(self, record):
-            pass
-
-logging.getLogger(__name__).addHandler(NullHandler())
